532ba4a1ffdbabcfeabcc513f316027d12185d26
hrpalencia <hrpmicarelli@gmail.com>
2022-01-30 21:23:05 -0400

    actualizacion

--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,31 @@ use near_sdk::{
     env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
 };
 
+use serde::Serialize;
+use serde::Deserialize;
+
 near_sdk::setup_alloc!();
+
+
+
+#[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct ProfileObjects {
+    user_id: AccountId,
+    name: String,
+    last_name: String,
+    email: String,
+    bio: String,
+    website: String,
+}
+
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
+    profile: Vec<ProfileObjects>,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -62,6 +80,7 @@ impl Contract {
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            profile: Vec::new(),
         }
     }
 
@@ -73,6 +92,47 @@ impl Contract {
         token_metadata: TokenMetadata,
     ) -> Token {
         self.tokens.mint(token_id, receiver_id, Some(token_metadata))
+    }
+
+    
+    pub fn set_profile(&mut self, name: String, last_name: String, email: String, bio: String, website: String) {
+        let mut duplicate: bool = false;
+        for i in 0..self.profile.len() {
+            if self.profile.get(i).unwrap().user_id == env::signer_account_id() {
+                duplicate = true;
+            }
+        }
+        if duplicate == false {  
+            let data = ProfileObjects {
+                user_id: env::signer_account_id(),
+                name: name.to_string(),
+                last_name: last_name.to_string(),
+                email: email.to_string(),
+                bio: bio.to_string(),
+                website: website.to_string(),
+            };
+            self.profile.push(data);
+            env::log(b"profile Created");
+        } else {
+            env::panic(b"profile already exists");
+        }
+    }
+
+    pub fn put_profile(&mut self, name: String, last_name: String, email: String, bio: String, website: String) -> String {
+        for i in 0..self.profile.len() {
+            if self.profile.get(i).unwrap().user_id == env::signer_account_id() {
+                self.profile[i].name = name.to_string();
+                self.profile[i].last_name = last_name.to_string();
+                self.profile[i].email = email.to_string();
+                self.profile[i].bio = bio.to_string();
+                self.profile[i].website = website.to_string();
+                env::log(b"profile Update");
+                return "profile Update".to_string();
+                break;
+            }
+        }
+        env::panic(b"user not found");
+        return "user not found".to_string();
     }
 }
 
@@ -88,7 +148,7 @@ impl NonFungibleTokenMetadataProvider for Contract {
 }
 
 
-/*
+
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
     use near_sdk::test_utils::{accounts, VMContextBuilder};
@@ -305,4 +365,3 @@ mod tests {
         assert!(!contract.nft_is_approved(token_id.clone(), accounts(1), Some(1)));
     }
 }
-*/
