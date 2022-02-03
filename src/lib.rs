4309d4ec5951c48051d25fbab3aea570101a4d30
hrpalencia <hrpmicarelli@gmail.com>
2022-02-03 14:29:57 -0400

    actualizar

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,58 @@
 
+/* use near_contract_standards::non_fungible_token::core::{
+    NonFungibleTokenCore, NonFungibleTokenResolver,
+}; */
 use near_contract_standards::non_fungible_token::metadata::{
     NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
 };
-use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_contract_standards::non_fungible_token::NonFungibleToken;
+use near_contract_standards::non_fungible_token::{Token, TokenId};
+use near_contract_standards::non_fungible_token::refund_deposit;
+
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::LazyOption;
 use near_sdk::json_types::ValidAccountId;
 use near_sdk::{
-    env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
+    env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue, Balance,
+    serde_json::json,
 };
+use near_sdk::collections::{LazyOption, UnorderedMap, UnorderedSet};
 
 use serde::Serialize;
 use serde::Deserialize;
+// use std::collections::HashMap;
+use near_sdk::env::is_valid_account_id;
+
+
+
 
 near_sdk::setup_alloc!();
 
+
+
+pub type TokenSeriesId = String;
+
+const MAX_PRICE: Balance = 1_000_000_000 * 10u128.pow(24);
+pub const TOKEN_DELIMETER: char = ':';
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct TokenSeries {
+	metadata: TokenMetadata,
+	creator_id: AccountId,
+	tokens: UnorderedSet<TokenId>,
+    price: Option<Balance>,
+    is_mintable: bool,
+    royalty: HashMap<AccountId, u32>
+}
+
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct TokenSeriesJson {
+    token_series_id: TokenSeriesId,
+	metadata: TokenMetadata,
+	creator_id: AccountId,
+    royalty: HashMap<AccountId, u32>
+}
 
 
 #[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
@@ -35,10 +72,13 @@ pub struct ProfileObjects {
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
+    // CUSTOM
+    token_series_by_id: UnorderedMap<TokenSeriesId, TokenSeries>,
     profile: Vec<ProfileObjects>,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
@@ -47,6 +87,10 @@ enum StorageKey {
     TokenMetadata,
     Enumeration,
     Approval,
+    // CUSTOM
+    TokenSeriesById,
+    TokensBySeriesInner { token_series: String },
+    TokensPerOwner { account_hash: Vec<u8> },
 }
 
 #[near_bindgen]
@@ -79,6 +123,7 @@ impl Contract {
                 Some(StorageKey::Enumeration),
                 Some(StorageKey::Approval),
             ),
+            token_series_by_id: UnorderedMap::new(StorageKey::TokenSeriesById),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
             profile: Vec::new(),
         }
@@ -94,7 +139,190 @@ impl Contract {
         self.tokens.mint(token_id, receiver_id, Some(token_metadata))
     }
 
+    #[payable]
+    pub fn nft_series(
+        &mut self,
+        creator_id: Option<ValidAccountId>,
+        token_metadata: TokenMetadata,
+        price: Option<U128>,
+        royalty: Option<HashMap<AccountId, u32>>,
+    ) -> TokenSeriesJson {
+        let initial_storage_usage = env::storage_usage();
+        let caller_id = env::predecessor_account_id();
+
+        if creator_id.is_some() {
+            assert_eq!(creator_id.unwrap().to_string(), caller_id, "Paras: Caller is not creator_id");
+        }
+
+        let token_series_id = format!("{}", (self.token_series_by_id.len() + 1));
+
+        assert!(
+            self.token_series_by_id.get(&token_series_id).is_none(),
+            "Paras: duplicate token_series_id"
+        );
+
+        let title = token_metadata.title.clone();
+        assert!(title.is_some(), "Paras: token_metadata.title is required");
+        
+
+        let mut total_perpetual = 0;
+        let mut total_accounts = 0;
+        let royalty_res: HashMap<AccountId, u32> = if let Some(royalty) = royalty {
+            for (k , v) in royalty.iter() {
+                if !is_valid_account_id(k.as_bytes()) {
+                    env::panic("Not valid account_id for royalty".as_bytes());
+                };
+                total_perpetual += *v;
+                total_accounts += 1;
+            }
+            royalty
+        } else {
+            HashMap::new()
+        };
+
+        assert!(total_accounts <= 10, "Paras: royalty exceeds 10 accounts");
+
+        assert!(
+            total_perpetual <= 9000,
+            "Paras Exceeds maximum royalty -> 9000",
+        );
+
+        let price_res: Option<u128> = if price.is_some() {
+            assert!(
+                price.unwrap().0 < MAX_PRICE,
+                "Paras: price higher than {}",
+                MAX_PRICE
+            );
+            Some(price.unwrap().0)
+        } else {
+            None
+        };
+
+        self.token_series_by_id.insert(&token_series_id, &TokenSeries{
+            metadata: token_metadata.clone(),
+            creator_id: caller_id.to_string(),
+            tokens: UnorderedSet::new(
+                StorageKey::TokensBySeriesInner {
+                    token_series: token_series_id.clone(),
+                }
+                .try_to_vec()
+                .unwrap(),
+            ),
+            price: price_res,
+            is_mintable: true,
+            royalty: royalty_res.clone(),
+        });
+
+        env::log(
+            json!({
+                "type": "nft_create_series",
+                "params": {
+                    "token_series_id": token_series_id,
+                    "token_metadata": token_metadata,
+                    "creator_id": caller_id,
+                    "price": price,
+                    "royalty": royalty_res
+                }
+            })
+            .to_string()
+            .as_bytes(),
+        );
+
+        refund_deposit(env::storage_usage() - initial_storage_usage);
+
+		TokenSeriesJson{
+            token_series_id,
+			metadata: token_metadata,
+			creator_id: caller_id.into(),
+            royalty: royalty_res,
+		}
+    }
     
+
+    #[payable]
+    pub fn nft_mint_series(
+        &mut self, 
+        token_series_id: TokenSeriesId, 
+        receiver_id: ValidAccountId
+    ) -> TokenId {
+        let initial_storage_usage = env::storage_usage();
+
+        let token_series = self.token_series_by_id.get(&token_series_id).expect("Paras: Token series not exist");
+        assert_eq!(env::predecessor_account_id(), token_series.creator_id, "Paras: not creator");
+        let token_id: TokenId = self._nft_mint_series(token_series_id, receiver_id.to_string());
+
+        refund_deposit(env::storage_usage() - initial_storage_usage);
+
+        token_id
+    }
+
+
+    fn _nft_mint_series(
+        &mut self, 
+        token_series_id: TokenSeriesId, 
+        receiver_id: AccountId
+    ) -> TokenId {
+        let mut token_series = self.token_series_by_id.get(&token_series_id).expect("Paras: Token series not exist");
+        assert!(
+            token_series.is_mintable,
+            "Paras: Token series is not mintable"
+        );
+
+        let num_tokens = token_series.tokens.len();
+        let max_copies = token_series.metadata.copies.unwrap_or(u64::MAX);
+        assert!(num_tokens < max_copies, "Series supply maxed");
+
+        if (num_tokens + 1) >= max_copies {
+            token_series.is_mintable = false;
+        }
+
+        let token_id = format!("{}{}{}", &token_series_id, TOKEN_DELIMETER, num_tokens + 1);
+        token_series.tokens.insert(&token_id);
+        self.token_series_by_id.insert(&token_series_id, &token_series);
+
+   
+        let metadata = Some(TokenMetadata {
+            title: None,          // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
+            description: None,    // free-form description
+            media: None, // URL to associated media, preferably to decentralized, content-addressed storage
+            media_hash: None, // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
+            copies: None, // number of copies of this set of metadata in existence when token was minted.
+            issued_at: Some(env::block_timestamp().to_string()), // ISO 8601 datetime when token was issued or minted
+            expires_at: None, // ISO 8601 datetime when token expires
+            starts_at: None, // ISO 8601 datetime when token starts being valid
+            updated_at: None, // ISO 8601 datetime when token was last updated
+            extra: None, // anything extra the NFT wants to store on-chain. Can be stringified JSON.
+            reference: None, // URL to an off-chain JSON file with more info.
+            reference_hash: None, // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
+        });
+
+        //let token = self.tokens.mint(token_id, receiver_id, metadata);
+        // From : https://github.com/near/near-sdk-rs/blob/master/near-contract-standards/src/non_fungible_token/core/core_impl.rs#L359
+        // This allows lazy minting
+
+        let owner_id: AccountId = receiver_id;
+        self.tokens.owner_by_id.insert(&token_id, &owner_id);
+
+        self.tokens
+            .token_metadata_by_id
+            .as_mut()
+            .and_then(|by_id| by_id.insert(&token_id, &metadata.as_ref().unwrap()));
+
+         if let Some(tokens_per_owner) = &mut self.tokens.tokens_per_owner {
+             let mut token_ids = tokens_per_owner.get(&owner_id).unwrap_or_else(|| {
+                 UnorderedSet::new(StorageKey::TokensPerOwner {
+                     account_hash: env::sha256(&owner_id.as_bytes()),
+                 })
+             });
+             token_ids.insert(&token_id);
+             tokens_per_owner.insert(&owner_id, &token_ids);
+         }
+
+
+        token_id
+    }
+
+
     pub fn set_profile(&mut self, name: String, last_name: String, email: String, bio: String, website: String) {
         let mut duplicate: bool = false;
         for i in 0..self.profile.len() {
