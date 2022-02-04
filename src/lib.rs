cc9ccc7572e54c8f27b82827a51407f89fd9fae3
hrpalencia <hrpmicarelli@gmail.com>
2022-02-04 10:25:35 -0400

    act

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,6 @@ pub struct TokenSeriesJson {
 #[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct ProfileObjects {
-    user_id: AccountId,
     name: String,
     last_name: String,
     email: String,
@@ -74,7 +73,7 @@ pub struct Contract {
     metadata: LazyOption<NFTContractMetadata>,
     // CUSTOM
     token_series_by_id: UnorderedMap<TokenSeriesId, TokenSeries>,
-    profile: Vec<ProfileObjects>,
+    profile: UnorderedMap<AccountId, ProfileObjects>,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -125,7 +124,7 @@ impl Contract {
             ),
             token_series_by_id: UnorderedMap::new(StorageKey::TokenSeriesById),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
-            profile: Vec::new(),
+            profile: UnorderedMap::new(b"s".to_vec()),
         }
     }
 
@@ -319,43 +318,46 @@ impl Contract {
     }
 
 
-    pub fn set_profile(&mut self, name: String, last_name: String, email: String, bio: String, website: String) {
+    pub fn set_profile(&mut self, name: String, last_name: String, email: String, bio: String, website: String) -> ProfileObjects {
         let mut duplicate: bool = false;
-        for i in 0..self.profile.len() {
-            if self.profile.get(i).unwrap().user_id == env::signer_account_id() {
-                duplicate = true;
-            }
+        let profile = self.profile.get(&env::signer_account_id());
+        if profile.is_none() {
+            duplicate = true;
         }
+
+        let data = ProfileObjects {
+            name: name,
+            last_name: last_name,
+            email: email,
+            bio: bio,
+            website: website,
+        };
         if duplicate == false {  
-            let data = ProfileObjects {
-                user_id: env::signer_account_id(),
-                name: name.to_string(),
-                last_name: last_name.to_string(),
-                email: email.to_string(),
-                bio: bio.to_string(),
-                website: website.to_string(),
-            };
-            self.profile.push(data);
-            env::log(b"profile Created");
+            self.profile.insert(&env::signer_account_id(), &data);
+            env::log(b"Profile Created");
+            data
         } else {
-            env::panic(b"profile already exists");
+            env::panic(b"Profile already exists");
         }
     }
 
-    pub fn put_profile(&mut self, name: String, last_name: String, email: String, bio: String, website: String) -> String {
-        for i in 0..self.profile.len() {
-            if self.profile.get(i).unwrap().user_id == env::signer_account_id() {
-                self.profile[i].name = name.to_string();
-                self.profile[i].last_name = last_name.to_string();
-                self.profile[i].email = email.to_string();
-                self.profile[i].bio = bio.to_string();
-                self.profile[i].website = website.to_string();
-                env::log(b"profile Update");
-                return "profile Update".to_string();
-            }
+    pub fn put_profile(&mut self, name: String, last_name: String, email: String, bio: String, website: String) -> ProfileObjects {
+        let mut profile = self.profile.get(&env::signer_account_id().to_string()).expect("Profile not exist");
+        profile.name = name.to_string();
+        profile.last_name = last_name.to_string();
+        profile.email = email.to_string();
+        profile.bio = bio.to_string();
+        profile.website = website.to_string();
+        
+        env::log(b"Profile Update");
+        
+        ProfileObjects {
+            name: name,
+            last_name: last_name,
+            email: email,
+            bio: bio,
+            website: website,
         }
-        env::panic(b"user not found");
-        return "user not found".to_string();
     }
 
 
@@ -370,22 +372,20 @@ impl Contract {
 		}
 	}
 
-    pub fn get_profile(&self) -> Vec<ProfileObjects> {
-        let mut result: Vec<ProfileObjects> = Vec::new();
-        for i in 0..self.profile.len() {
-            if self.profile[i].user_id == env::signer_account_id() {
-                result.push(ProfileObjects {
-                    user_id: self.profile[i].user_id.to_string(),
-                    name: self.profile[i].name.to_string(),
-                    last_name: self.profile[i].last_name.to_string(),
-                    email: self.profile[i].email.to_string(),
-                    bio: self.profile[i].bio.to_string(),
-                    website: self.profile[i].website.to_string(),
-                });
-            }
+    pub fn get_profile(&self) -> ProfileObjects {
+        let profile = self.profile.get(&env::signer_account_id()).expect("Profile not exist");
+        
+        ProfileObjects {
+            name: profile.name,
+            last_name: profile.last_name,
+            email: profile.email,
+            bio: profile.bio,
+            website: profile.website,
         }
-        result
 	}
+
+
+
 }
 
 near_contract_standards::impl_non_fungible_token_core!(Contract, tokens);
